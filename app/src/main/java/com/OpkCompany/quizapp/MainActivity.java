e2f1af7078b8861ad37b15b0ae49b4d0c5ba0afa
owdenpk <owden.kimaro@riarauniversity.ac.ke>
2019-08-17 19:56:05 +0300

    First commit

---
+++ app/src/main/java/com/OpkCompany/quizapp/MainActivity.java
@@ -0,0 +1,209 @@
+package com.OpkCompany.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.invertemotech.quizapp.R;
+
+import java.util.ArrayList;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView questionLabel, questionCountLabel, scoreLabel;
+    EditText answerEdt;
+    Button submitButton;
+    ProgressBar progressBar;
+    ArrayList<QuestionModel> questionModelArraylist;
+
+
+    int currentPosition = 0;
+    int numberOfCorrectAnswer = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        questionCountLabel = findViewById(R.id.noQuestion);
+        questionLabel = findViewById(R.id.question);
+        scoreLabel = findViewById(R.id.score);
+
+        answerEdt = findViewById(R.id.answer);
+        submitButton = findViewById(R.id.submit);
+        progressBar = findViewById(R.id.progress);
+
+        questionModelArraylist = new ArrayList<>();
+
+        setUpQuestion();
+
+        setData();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                checkAnswer();
+            }
+        });
+
+        answerEdt.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                Log.e("event.getAction()",event.getAction()+"");
+                Log.e("event.keyCode()",keyCode+"");
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                    checkAnswer();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+    public void checkAnswer(){
+        String answerString  = answerEdt.getText().toString().trim();
+
+
+
+
+        if(answerString.equalsIgnoreCase(questionModelArraylist.get(currentPosition).getAnswer())){
+            numberOfCorrectAnswer ++;
+
+
+
+            new SweetAlertDialog(MainActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                    .setTitleText("Good job!")
+                    .setContentText("Right Asswer")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                            currentPosition ++;
+
+                            setData();
+                            answerEdt.setText("");
+                            sweetAlertDialog.dismiss();
+                        }
+                    })
+                    .show();
+
+
+        }else {
+
+            new SweetAlertDialog(MainActivity.this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Wrong Answer")
+                    .setContentText("The right answer is : "+questionModelArraylist.get(currentPosition).getAnswer())
+                    .setConfirmText("OK")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+                            sDialog.dismiss();
+
+                            currentPosition ++;
+
+                            setData();
+                            answerEdt.setText("");
+                        }
+                    })
+                    .show();
+        }
+
+
+
+
+
+        int x = ((currentPosition+1) * 100) / questionModelArraylist.size();
+
+        progressBar.setProgress(x);
+
+
+
+    }
+
+
+
+
+    public void setUpQuestion(){
+
+
+        questionModelArraylist.add(new QuestionModel("Which continent is Faroe islands part of ? ","Europe"));
+        questionModelArraylist.add(new QuestionModel("What is the name of android 7.0? ","Nougat"));
+        questionModelArraylist.add(new QuestionModel("What is core component of Android OS, " +
+                "that allows users to coordinate the functions of different activities to achieve a task. ? ","Intent"));
+        questionModelArraylist.add(new QuestionModel("Linux kernel provides multi-tasking execution environment " +
+                "so that multiple processes can execute each process runs on its own instance of Android Runtime (ART). ? ","Android Runtime"));
+        questionModelArraylist.add(new QuestionModel("Is an open source operating system and is mainly popular for Smartphones and Tablets. ? ",
+                "Android"));
+        questionModelArraylist.add(new QuestionModel("a collection of views and other child views, it is an invisible part and the base class for layouts. ? ",
+                "View group"));
+        questionModelArraylist.add(new QuestionModel("It is acts as bridge between emulator and IDE, it executes remote shell commands to run applications on an emulator ? ",
+                "ADB"));
+        questionModelArraylist.add(new QuestionModel(" It holds objects,widgets,labels,fields,icons,buttons.etc ? ",
+                "container"));
+        questionModelArraylist.add(new QuestionModel("The Android packaging key is compressed with classes,UI's, supportive assets and manifest to a single file ? ",
+                "APK"));
+        questionModelArraylist.add(new QuestionModel("Which Kernel is used in Android operating system? ",
+                "Linux Kernel"));
+
+
+
+    }
+
+    public void setData(){
+
+
+        if(questionModelArraylist.size()>currentPosition) {
+
+            questionLabel.setText(questionModelArraylist.get(currentPosition).getQuestionString());
+
+            scoreLabel.setText("Score :" + numberOfCorrectAnswer + "/" + questionModelArraylist.size());
+            questionCountLabel.setText("Question No : " + (currentPosition + 1));
+
+
+        }else{
+
+
+            new SweetAlertDialog(MainActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                    .setTitleText("You have successfully completed the quiz")
+                    .setContentText("Your score is : "+ numberOfCorrectAnswer + "/" + questionModelArraylist.size())
+                    .setConfirmText("Restart")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+
+                            sDialog.dismissWithAnimation();
+                            currentPosition = 0;
+                            numberOfCorrectAnswer = 0;
+                            progressBar.setProgress(0);
+                            setData();
+                        }
+                    })
+                    .setCancelText("Close")
+                    .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+
+                            sDialog.dismissWithAnimation();
+                            finish();
+                        }
+                    })
+                    .show();
+
+        }
+
+    }
+
+
+
+}
